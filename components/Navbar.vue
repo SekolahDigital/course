c85fc9d88faa9ce1d502ed7762bf18840caa2aba
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-11-26 15:22:47 +0700

    app - add vue carousel

---
+++ components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+   <nav class="navbar navbar-expand-lg navbar-dark">
+      <router-link class="navbar-brand" to="/">
+        <h4 class="font-weight-bold">Sekolah<br>Digital</h4>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-5">
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/kelas">KELAS</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/mading">MADING</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/roadmap">ROADMAP</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/cart">CART</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/wishlist">WISHLIST</router-link>
+          </li>
+    
+          <li class="nav-item login-nav">
+            <router-link
+              class="nav-link btn btn-dark login font-weight-bold"
+              active-class="active"
+              to="/login"
+            >LOGIN</router-link>
+            
+          </li>
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+ name: "Navbar"
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,72 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1>Halo, Selamat Datang</h1>
-      <NuxtLink to="test">Test-to</NuxtLink>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+<main id="app">
+  <section>
+    <div class="container">
+      <Navbar/>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mt-5" style="margin-top:100px">
+          <h1 class="text-redhat">Buat Mimpimu Jadi Kenyataan.</h1>
+          <p class="text-redhat" style="width:580px;font-size:24px">Pelajari kelas-kelas terbaik yang kita sediakan <br> untuk menjenjang karir atau sekedar membuat karya. kami tunggu dikelas!</p>
+           <input
+              type="text"
+              class="form-control border border-dark py-4 px-4 col-lg-9 col-12 mt-5"
+              placeholder="Cari kelas ex: Javascript"
+            />
+        </div>
+        <div class="col-lg-4">
+          <img src="~/assets/svg/home_bottom.svg" class="position-absolute" style="top:-100px;left:-70px">
+          <img src="~/assets/svg/home.svg" class="position-absolute">
+        </div>
       </div>
     </div>
-  </div>
+  </section>
+
+  <section style="margin-top: 400px" class="mb-5">
+    <div class="container">
+      <h2 class="font-weight-bold">Kelas Rekomendasi</h2>
+      <p>Rekomendasi kami untuk kamu yang suka belajar hal baru.</p>
+      <no-ssr placeholder="Loading...">
+        <carousel :per-page="4" >
+          <slide>
+            <DaftarKelas/>
+          </slide>
+          <slide>
+            <DaftarKelas/>
+          </slide>
+          <slide>
+            <DaftarKelas/>
+          </slide>
+          <slide>
+            <DaftarKelas/>
+          </slide>
+          <slide>
+            <DaftarKelas/>
+          </slide>
+          <slide>
+            <DaftarKelas/>
+          </slide>
+          <slide>
+            <DaftarKelas/>
+          </slide>
+        </carousel>
+      </no-ssr>
+    </div>
+  </section>
+</main>
 </template>
 
 <script>
-export default {}
+import "~/assets/style.css"
+// import {Carousel,Slide} from "vue-carousel"
+export default {
+ 
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
