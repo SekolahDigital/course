30d29824fa82260fd889c0f493471859cb7cd437
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-12-14 13:50:53 +0700

    app - finish template

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -27,8 +27,6 @@
                       <img src="https://scontent-xsp1-2.cdninstagram.com/v/t51.2885-15/e35/67632001_475641503272345_8035766879171348296_n.jpg?_nc_ht=scontent-xsp1-2.cdninstagram.com&_nc_cat=101&_nc_ohc=pT7e4dXl_bwAX-4PSrh&tp=1&oh=dac6f5829afcb9a26e12fdd177c42907&oe=5FFF5AE3" alt="" class="rounded-circle img-fluid" width="50">
                       <p
                         class="nav-link font-weight-bold my-0"
-                        active-class="active"
-                        to="/login"
                       >Rafli Ramadhan</p>
                     </div>
                   </li>
@@ -37,10 +35,10 @@
             </nav>
             <section>
               <div class="container">
-                <h4 class="mb-0">Progress Kelas</h4>
-                <p>ini adalah kelas yang kamu ikuti</p>
+                <h4 class="mb-0 font-weight-600">Progress Kelas</h4>
+                <p class="text-muted">ini adalah kelas yang kamu ikuti</p>
                 <div class="row">
-                  <div class="col-5">
+                  <div class="col-6">
                     <div class="item-kelas py-2 pb-3 border bg-white rounded px-4" v-for="kelas in 3" :key="kelas.index">
                       <p class="mb-2 py-2 font-weight-600">Fullstack Developer</p>
                       <div class="progress mb-3">
@@ -62,9 +60,19 @@
                       </span>
                     </div>
                   </div>
-                  <div class="col-1"></div>
-                  <div class="col-5">
-                    asd
+                 <div class="offset-lg-1 col-lg-5">
+                    <div>
+                      <h5 class="font-weight-600 mb-0">Terakhir Dipelajari</h5>
+                      <p class="text-muted">Ayo Selesaikan kelasmu</p>
+                    </div>
+                     <div class="item-kelas history py-2 pb-3 border rounded px-4 bg-white">
+                        <p class="font-weight-600 mb-0">Installasi Tools</p>
+                        <p class="text-muted mb-2">Fullstack Developer</p>
+                        <router-link
+                          to="a"
+                          class="btn mr-2 btn-sm btn-dark px-4 font-weight-600"
+                        >LANJUT</router-link>
+                      </div>
                   </div>
                 </div>
               </div>
